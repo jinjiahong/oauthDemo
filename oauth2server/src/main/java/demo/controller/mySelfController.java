5d231c66f5f62a625f66f87bfdc38378563a0431
jinjiahong <771417125@qq.com>
2017-04-12 17:29:32 +0800

    去除不必要的引入

--- oauth2server/src/main/java/demo/controller/mySelfController.java
+++ oauth2server/src/main/java/demo/controller/mySelfController.java
@@ -1,7 +1,5 @@
 package demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
